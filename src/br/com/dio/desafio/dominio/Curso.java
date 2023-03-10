c7a036c813058784456cad70fa5240b29444e49d
Gabriel Gomes <gabriel.grohl2006@gmail.com>
2023-03-10 06:41:10 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,38 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+
+    private String título;
+    private String descricao;
+    private int cargaHoraria;
+
+    public String getTítulo() {
+        return título;
+    }
+
+    public void setTítulo(String título) {
+        this.título = título;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+
+    }
+
+    public Curso() {
+
+
+    }
+}
